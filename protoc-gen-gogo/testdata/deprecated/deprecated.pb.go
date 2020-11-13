b892c1b298f762ceb8ab2e756e5a0016ed0a6ec0
Malte Isberner <2822367+misberner@users.noreply.github.com>
2020-11-13 16:13:18 +0100

    Support for client conn interface (#4)

--- protoc-gen-gogo/testdata/deprecated/deprecated.pb.go
+++ protoc-gen-gogo/testdata/deprecated/deprecated.pb.go
@@ -192,15 +192,15 @@ var fileDescriptor_f64ba265cd7eae3f = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConn
+var _ grpc.ClientConnInterface
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion4
+const _ = grpc.SupportPackageIsVersion6
 
 // DeprecatedServiceClient is the client API for DeprecatedService service.
 //
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConnInterface.NewStream.
 //
 // Deprecated: Do not use.
 type DeprecatedServiceClient interface {
@@ -209,11 +209,11 @@ type DeprecatedServiceClient interface {
 }
 
 type deprecatedServiceClient struct {
-	cc *grpc.ClientConn
+	cc grpc.ClientConnInterface
 }
 
 // Deprecated: Do not use.
-func NewDeprecatedServiceClient(cc *grpc.ClientConn) DeprecatedServiceClient {
+func NewDeprecatedServiceClient(cc grpc.ClientConnInterface) DeprecatedServiceClient {
 	return &deprecatedServiceClient{cc}
 }
 
